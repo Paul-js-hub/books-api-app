72eaf4c484d5bdab9b90637ebd3c14e7d5245939
Paul Onchera <hesbonpoisson@gmail.com>
2020-05-18 19:38:01 +0300

    Registering a user

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import dbConnection from './mongodb';
 import Book from './bookModel';
-const Joi = require('joi');
-const express = require('express');
-const cors = require('cors');
+import User from './user';
+import Joi from '@hapi/joi';
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 // connect db
@@ -17,16 +18,16 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/books', async (req, res) => {
-    try{
+    try {
         const books = await Book.find();
         return res.send(books);
-    } catch (err){
+    } catch (err) {
         res.json({ message: err })
     }
-    
+
 });
 
-app.get('/api/books/:id', async(req, res) => {
+app.get('/api/books/:id', async (req, res) => {
     //return a particular book with an id
     const book = await Book.findById(req.params.id); // Looking up for the book
     if (!book) res.status(404).send('The book with that particular ID not found.'); ////If it does not exist return Not Found 
@@ -36,10 +37,10 @@ app.get('/api/books/:id', async(req, res) => {
 app.post('/api/books', (req, res) => {
     // Validating the request body using joi
     console.log("request", req.body);
-    const schema = {
+    const schema = Joi.object({
         title: Joi.string().min(3).required()
-    };
-    const result = Joi.validate(req.body, schema);
+    });
+    const result = schema.validate(req.body);
 
     // If invalid return a 400 error
     if (result.error) {
@@ -51,22 +52,22 @@ app.post('/api/books', (req, res) => {
     };
 
     let model = new Book(book);
-    model.save().then((doc)=>{
+    model.save().then((doc) => {
         res.status(201).send(doc)
-    }).catch((err)=>{
+    }).catch((err) => {
         res.status(500).send(err)
     })
 });
 
-app.put('/api/books/:id', async (req, res) =>{
+app.put('/api/books/:id', async (req, res) => {
 
     const book = await Book.updateOne(
-        {_id:req.params.id},
-        {$set:{title:req.body.title}}
-        ); 
-    if (!book) res.status(404).send('The book with that particular ID not found.'); 
+        { _id: req.params.id },
+        { $set: { title: req.body.title } }
+    );
+    if (!book) res.status(404).send('The book with that particular ID not found.');
 
-    const {error} = validateBook(req.body); // result.error
+    const { error } = validateBook(req.body); // result.error
     if (error) {
         res.status(400).send(error.details[0].message);
         return;
@@ -75,18 +76,52 @@ app.put('/api/books/:id', async (req, res) =>{
     return res.status(201).send(book);
 });
 
-app.delete('/api/books/:id', async(req, res) => {
-    const book = await Book.deleteOne({_id:req.params.id}); 
-    if (!book) res.status(404).send('The book with that particular ID not found.'); 
+app.delete('/api/books/:id', async (req, res) => {
+    const book = await Book.deleteOne({ _id: req.params.id });
+    if (!book) res.status(404).send('The book with that particular ID not found.');
 
     return res.status(200).send(book);
 });
 
-function validateBook(book){
+
+//REGISTER
+const schema = Joi.object({
+    email: Joi.string().min(6).required().email(),
+    password:Joi.string().min(6).required()
+});
+app.post('/api/register', async(req, res) => {
+
+    //validation
+    const {error}= schema.validate(req.body);
+    if(error){
+        return res.status(400).send(error.details[0].message)
+    }
+
+    // If email exists
+    const emailExists = await User.findOne({email:req.body.email});
+    if(emailExists) res.status(400).send('Email already exists');
+    
+    //create new user
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    
+        await user.save()
+        .then((doc) => {
+            res.status(201).send(doc)
+        }).catch((err) => {
+            res.status(500).send(err)
+        })
+    
+});
+
+function validateBook(book) {
     const schema = {
         title: Joi.string().min(3).required()
     };
-  return Joi.validate(book, schema);
+    return Joi.validate(book, schema);
 }
-    
+
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
