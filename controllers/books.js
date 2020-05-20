71d9b9afa3d6f40171289cdcfc870359388a63d3
Paul Onchera <hesbonpoisson@gmail.com>
2020-05-20 17:31:51 +0300

    "created book api controller"

---
+++ controllers/books.js
@@ -0,0 +1,73 @@
+import Book from '../bookModel';
+import Joi from '@hapi/joi';
+
+exports.books_get_all = async (req, res) => {
+    try {
+        const books = await Book.find();
+        return res.send(books);
+    } catch (err) {
+        res.json({ message: err })
+    }
+
+}
+exports.books_get_byId = async (req, res) => {
+    //return a particular book with an id
+    const book = await Book.findById(req.params.id); // Looking up for the book
+    if (!book) res.status(404).send('The book with that particular ID not found.'); ////If it does not exist return Not Found 
+    res.send(book);
+}
+exports.books_create = (req, res) => {
+    // Validating the request body using joi
+    console.log("request", req.body);
+    const schema = Joi.object({
+        title: Joi.string().min(3).required()
+    });
+    const result = schema.validate(req.body);
+
+    // If invalid return a 400 error
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    const book = {
+        title: req.body.title
+    };
+
+    let model = new Book(book);
+    model.save().then((doc) => {
+        res.status(201).send(doc)
+    }).catch((err) => {
+        res.status(500).send(err)
+    })
+}
+
+exports.books_update_byId = async (req, res) => {
+
+    const book = await Book.updateOne(
+        { _id: req.params.id },
+        { $set: { title: req.body.title } }
+    );
+    if (!book) res.status(404).send('The book with that particular ID not found.');
+
+    const { error } = validateBook(req.body); // result.error
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    // returning the updated book to the client
+    return res.status(201).send(book);
+}
+
+exports.books_delete_byId = async (req, res) => {
+    const book = await Book.deleteOne({ _id: req.params.id });
+    if (!book) res.status(404).send('The book with that particular ID not found.');
+
+    return res.status(200).send(book);
+}
+
+function validateBook(book) {
+    const schema = Joi.object({
+        title: Joi.string().min(3).required()
+    });
+    return schema.validate(book);
+}
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cors from 'cors';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-
+import booksController from './controllers/books'
 
 dotenv.config();
 const app = express();
@@ -22,71 +22,15 @@ app.get('/', (req, res) => {
     res.send('Hello Api!!!');
 });
 
-app.get('/api/books', verifyToken, async (req, res) => {
-    try {
-        const books = await Book.find();
-        return res.send(books);
-    } catch (err) {
-        res.json({ message: err })
-    }
+app.get('/api/books', verifyToken, booksController.books_get_all);
 
-});
+app.get('/api/books/:id',verifyToken, booksController.books_get_byId);
 
-app.get('/api/books/:id',verifyToken, async (req, res) => {
-    //return a particular book with an id
-    const book = await Book.findById(req.params.id); // Looking up for the book
-    if (!book) res.status(404).send('The book with that particular ID not found.'); ////If it does not exist return Not Found 
-    res.send(book);
-});
+app.post('/api/books', verifyToken, booksController.books_create);
 
-app.post('/api/books', verifyToken, (req, res) => {
-    // Validating the request body using joi
-    console.log("request", req.body);
-    const schema = Joi.object({
-        title: Joi.string().min(3).required()
-    });
-    const result = schema.validate(req.body);
+app.put('/api/books/:id', booksController.books_update_byId);
 
-    // If invalid return a 400 error
-    if (result.error) {
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
-    const book = {
-        title: req.body.title
-    };
-
-    let model = new Book(book);
-    model.save().then((doc) => {
-        res.status(201).send(doc)
-    }).catch((err) => {
-        res.status(500).send(err)
-    })
-});
-
-app.put('/api/books/:id', async (req, res) => {
-
-    const book = await Book.updateOne(
-        { _id: req.params.id },
-        { $set: { title: req.body.title } }
-    );
-    if (!book) res.status(404).send('The book with that particular ID not found.');
-
-    const { error } = validateBook(req.body); // result.error
-    if (error) {
-        res.status(400).send(error.details[0].message);
-        return;
-    }
-    // returning the updated book to the client
-    return res.status(201).send(book);
-});
-
-app.delete('/api/books/:id', async (req, res) => {
-    const book = await Book.deleteOne({ _id: req.params.id });
-    if (!book) res.status(404).send('The book with that particular ID not found.');
-
-    return res.status(200).send(book);
-});
+app.delete('/api/books/:id', booksController.books_delete_byId);
 
 
 //REGISTER
@@ -142,19 +86,14 @@ app.post('/api/login', async (req, res) => {
     const match = await bcrypt.compare(req.body.password, user.password);
     if (!match) return res.status(400).send('Invalid login credentials');
     if (match) {
-        const accessToken = jwt.sign({ email: user.email }, process.env.TOKEN_SECRET, { expiresIn: '10m' });
+        const accessToken = jwt.sign({ email: user.email }, process.env.TOKEN_SECRET, { expiresIn: '30m' });
         return res.status(201)
             .send({ message: 'Login Success', accessToken });
     }
     return res.send('An error occurred').status(500)
 });
 
-function validateBook(book) {
-    const schema = {
-        title: Joi.string().min(3).required()
-    };
-    return Joi.validate(book, schema);
-}
+
 //middleware
 function verifyToken(req, res, next) {
     const accessToken = req.header('auth-token');
