bbb43341ffc60580bdbb842fd67d6e9f8f166e18
Paul Onchera <hesbonpoisson@gmail.com>
2020-06-01 12:56:01 +0300

    added return to next in the middleware

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -2,16 +2,16 @@ import jwt from 'jsonwebtoken';
 
 
 //middleware
-module.exports = (req, res, next) =>{
-    const accessToken = req.header('auth-token');
+module.exports = async(req, res, next) =>{
+    const accessToken = await req.header('auth-token');
     if (!accessToken) {
         return res.status(401).send('Access Denied');
     }
     try {
         const verified = jwt.verify(accessToken, process.env.TOKEN_SECRET);
         req.u = verified;
-        next();
+        return next();
     } catch (error) {
-        res.status(403).send('Invalid Token')
+        return res.status(403).send('Invalid Token')
     }
 }
