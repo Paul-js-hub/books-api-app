2a63c05dfd8a906da156b5bb30cadbd904194cb5
Paul Onchera <hesbonpoisson@gmail.com>
2020-06-08 22:17:15 +0300

    added message key value userauthentication file

--- controllers/userAuthentication.js
+++ controllers/userAuthentication.js
@@ -10,19 +10,19 @@ import bcrypt from 'bcryptjs';
 //REGISTER
 const schema = Joi.object({
     email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required()
+    password: Joi.string().min(6).required(),
 });
 exports.user_register = async (req, res) => {
 
     //validation
     const { error } = schema.validate(req.body);
     if (error) {
-        return res.status(400).send(error.details[0].message)
+        return res.status(400).send({message:error.details[0].message})
     }
 
     // If email exists
     const emailExists = await User.findOne({ email: req.body.email });
-    if (emailExists) res.status(400).send('Email already exists');
+    if (emailExists) res.status(400).send({message:'Email already exists'});
 
     //hash passwords
     const salt = await bcrypt.genSalt(10);
@@ -50,20 +50,20 @@ const schemaLogin = Joi.object({
 
 exports.user_login = async (req, res) => {
     const { error } = schemaLogin.validate(req.body);
-    if (error) return res.status(422).send(error)
+    if (error) return res.status(422).send({message:error.details[0].message})
 
     //check if the user exists
     const user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(400).send("User with the email doesn't exist");
+    if (!user) return res.status(400).send({message:"User with the email doesn't exist"});
 
     //Check if the passwords match
     const match = await bcrypt.compare(req.body.password, user.password);
-    if (!match) return res.status(400).send('Invalid login credentials');
+    if (!match) return res.status(400).send({message:'Invalid login credentials'});
     if (match) {
         const accessToken = jwt.sign({ email: user.email }, process.env.TOKEN_SECRET, { expiresIn: '30m' });
         return res.status(201)
-            .send({ message: 'Login Success', accessToken });
+            .send({ message: 'Logged In Successfully', accessToken });
     }
-    return res.send('An error occurred').status(500)
+    return res.send({message:'An error occurred'}).status(500)
 }
 
